02f183879c2359d921e8b46157a118427cf49312
Rob Lewis <rob.y.lewis@gmail.com>
2020-05-20 12:12:03 +0200

    test again

--- html/redirect.js
+++
@@ -1,4 +0,0 @@
-var obj = JSON.parse(json_str);
-var urlParams = new URLSearchParams(window.location.search);
-var declName = urlParams.get('decl')
-window.location.replace(obj[declName] + '#' + declName);
